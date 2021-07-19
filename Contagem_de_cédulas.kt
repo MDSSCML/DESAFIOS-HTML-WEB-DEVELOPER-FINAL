d1c90b768631d46fd0b1a86326f15636796b605a
Márcio Trovão <mc872684@gmail.com>
2021-07-18 23:27:10 -0300

    Modificado algumas linhas de código

--- Contagem_de_cédulas.kt
+++ Contagem_de_cédulas.kt
@@ -1,16 +1,15 @@
 Contagem de cédulas
 
-let valor_entrada = parseInt(gets());
+let valor_entrada = +gets();
 
 const cedulas = [100, 50, 20, 10, 5, 2, 1];
 
-console.log(valor_entrada);
+console.log(`${valor_entrada}`);
 
 for (let cedula of cedulas) {
-    let qtd_cedula = parseInt(valor_entrada / cedula);
+         let  qtd_cedula = parseInt(`${valor_entrada / cedula}`);
+         
+    console.log(`${qtd_cedula} nota(s) de R$ ${cedula},00`);
 
-    console.log(qtd_cedula + " nota(s) de R$ " + cedula + ",00 ");
-
-    valor_entrada %= cedula;
-
+  valor_entrada %= cedula;
 }
