0797c32d34a45f19fd3b28832bb907e54ddafa50
Márcio Trovão <mc872684@gmail.com>
2021-07-21 12:47:28 -0300

    Adicionado versão while

--- Quantidades_de_números_positivos.kt
+++ Quantidades_de_números_positivos.kt
@@ -5,3 +5,17 @@ for (var i = 0 ; i <= 6; i++) {
  +gets() > 0 ? positivos++ : "Márcio trovão";
 }
 console.log(positivos + ' valores positivos');
+
+
+
+versão while
+
+let total = 0;
+let i = 0;
+let valor;
+
+while(i < 6){
+  +gets() > 0 ? total += 1 : "MT";
+    i++; 
+}
+  console.log(total + " valores positivos");
